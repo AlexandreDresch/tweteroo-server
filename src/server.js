f773ba11d33796edf8d717f30b2ee7587c5ac3a8
AlexandreDresch <z3r0pc@protonmail.com>
2023-01-06 23:32:08 -0300

    post tweet & get tweets update

--- src/server.js
+++ src/server.js
@@ -9,17 +9,48 @@ server.use(express.json());
 
 const PORT = 5000;
 
+let userData = {};
 const usersData = [];
+const tweetsData = [];
 
 server.post("/sign-up", (req, res) => {
   let { username, avatar } = req.body;
-  let userData = {
+  let user = {
     username,
     avatar,
   };
-  usersData.push(userData);
+  userData = { ...user };
+  usersData.push(user);
 
   res.send("OK");
+});
+
+server.post("/tweets", (req, res) => {
+  let { username, tweet } = req.body;
+
+  let user = usersData.find(user => user.username === username)
+  
+  if(!user) {
+    return res.send("UNAUTHORIZED")
+  }
+
+  let tweetData = {
+    username,    
+    avatar: userData.avatar,
+    tweet,
+  };
+  tweetsData.push(tweetData);
+
+  res.send("OK");
+});
+
+server.get("/tweets", (req, res) => {
+  if(tweetsData.length === 0) {
+  return  res.send(tweetsData);
+  }
+  let lastTenTweets = tweetsData.slice(-10).reverse();
+
+  res.send(lastTenTweets);
 });
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
