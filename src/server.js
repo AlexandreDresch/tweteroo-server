8c7e9840e930c8ffa91dffcb11cf422992ebc322
AlexandreDresch <z3r0pc@protonmail.com>
2023-01-09 11:24:49 -0300

    sign-in & tweet validation update

--- src/server.js
+++ src/server.js
@@ -15,38 +15,56 @@ const tweetsData = [];
 
 server.post("/sign-up", (req, res) => {
   let { username, avatar } = req.body;
+
+  if (
+    !username ||
+    !avatar ||
+    typeof username !== "string" ||
+    typeof avatar !== "string" ||
+    username === "" ||
+    avatar === "" ||
+    avatar === ""
+  ) {
+    return res.status(400).send("Todos os campos são obrigatórios!");
+  }
+
   let user = {
     username,
     avatar,
   };
+
   userData = { ...user };
   usersData.push(user);
 
-  res.send("OK");
+  res.status(201).send("OK");
 });
 
 server.post("/tweets", (req, res) => {
   let { username, tweet } = req.body;
 
-  let user = usersData.find(user => user.username === username)
-  
-  if(!user) {
-    return res.send("UNAUTHORIZED")
+  let user = usersData.find((user) => user.username === username);
+
+  if (!user) {
+    return res.status(401).send("UNAUTHORIZED");
+  }
+
+  if (!tweet || typeof tweet !== "string" || tweet === "") {
+    return res.status(400).send("Todos os campos são obrigatórios!");
   }
 
   let tweetData = {
-    username,    
+    username,
     avatar: userData.avatar,
     tweet,
   };
   tweetsData.push(tweetData);
 
-  res.send("OK");
+  res.status(201).send("OK");
 });
 
 server.get("/tweets", (req, res) => {
-  if(tweetsData.length === 0) {
-  return  res.send(tweetsData);
+  if (tweetsData.length === 0) {
+    return res.send(tweetsData);
   }
   let lastTenTweets = tweetsData.slice(-10).reverse();
 
